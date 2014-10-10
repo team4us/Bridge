ff84dd179053c2fac64a9364c297bee83d4e7684
Xiaohui Chen <xh_chen@DST54920.local>
2014-10-10 17:41:15 +0800

    添加orm和time第三方库

--- app/src/main/java/com/xiaohui/bridge/business/bean/Project.java
+++ app/src/main/java/com/xiaohui/bridge/business/bean/Project.java
@@ -5,7 +5,17 @@ package com.xiaohui.bridge.business.bean;
  */
 public class Project {
 
-    private String name;
+    private String code; //项目编号
+    private String name; //项目名称
+    private String company; //委托单位
+    private String category; //行业分类
+    private String manager; //项目经理
+    private String creator; //项目创建人
+    private String auditor; //审核人
+    private String judge; //审定人
+    private String decisionMaker; //决策人
+    private String member; //项目成员
+    private String decription; //项目概况
 
     public Project(String name) {
         this.name = name;
@@ -13,5 +23,62 @@ public class Project {
 
     public String getName() {
         return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public String getAuditor() {
+        return auditor;
+    }
+
+    public String getJudge() {
+        return judge;
+    }
+
+    public String getDecisionMaker() {
+        return decisionMaker;
+    }
+
+    public String getMember() {
+        return member;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", company='" + company + '\'' +
+                ", category='" + category + '\'' +
+                ", manager='" + manager + '\'' +
+                ", creator='" + creator + '\'' +
+                ", auditor='" + auditor + '\'' +
+                ", judge='" + judge + '\'' +
+                ", decisionMaker='" + decisionMaker + '\'' +
+                ", member='" + member + '\'' +
+                ", decription='" + decription + '\'' +
+                '}';
     }
 }
