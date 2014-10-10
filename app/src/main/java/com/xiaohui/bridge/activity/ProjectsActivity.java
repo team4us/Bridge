8eaef588cb9fbb2f5df587026fe46b9b13def2e6
Xiaohui Chen <xh_chen@DST54920.local>
2014-10-10 17:16:54 +0800

    调整部分icon资源

--- app/src/main/java/com/xiaohui/bridge/activity/ProjectsActivity.java
+++ app/src/main/java/com/xiaohui/bridge/activity/ProjectsActivity.java
@@ -3,7 +3,8 @@ package com.xiaohui.bridge.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.xiaohui.bridge.Keys;
 import com.xiaohui.bridge.R;
@@ -33,5 +34,20 @@ public class ProjectsActivity extends AbstractActivity implements IProjectView {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.project_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_download) {
+            Toast.makeText(this, R.string.action_download, Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_upload) {
+            Toast.makeText(this, R.string.action_upload, Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
