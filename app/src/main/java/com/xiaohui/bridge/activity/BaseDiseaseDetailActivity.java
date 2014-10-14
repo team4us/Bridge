3edf4def3cb6cece1ab81ebfad078d062ecb68f7
Jazoon <orosem625@163.com>
2014-10-15 01:43:42 +0800

    修正命名

--- app/src/main/java/com/xiaohui/bridge/activity/BaseDiseaseDetailActivity.java
+++ app/src/main/java/com/xiaohui/bridge/activity/BaseDiseaseDetailActivity.java
@@ -68,8 +68,8 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
     private RadioGroup rgRadioGroup;
     private LinearLayout llInputTemplate;
 
-    private MyGridView noScrollMyGridView;
-    private GridAdapter adapter;
+    private MyGridView mgvPicturesGridView;
+    private GridAdapter mgvPicturesAdapter;
 
     private String currentTakePictureName = "";
     private String path = "";
@@ -91,6 +91,12 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
         rgRadioGroup = (RadioGroup) findViewById(R.id.rg_radio_group);
         llInputTemplate = (LinearLayout) findViewById(R.id.ll_input_container);
 
+        initDiseaseDetailView();
+        initMediaLayout();
+        initGridView();
+    }
+
+    private void initDiseaseDetailView(){
         ArrayAdapter<String> positions = new ArrayAdapter<String>(this, R.layout.view_spinner_item, StoreManager.Instance.generalsTypes);
         positions.setDropDownViewResource(R.layout.view_spinner_dropdown_item);
         spChoosePosition.setAdapter(positions);
@@ -149,15 +155,15 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
                 }
             }
         });
+    }
 
-        initMediaLayout();
-
-        noScrollMyGridView = (MyGridView) findViewById(R.id.noScrollgridview);
-        noScrollMyGridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
-        adapter = new GridAdapter(this);
-        adapter.update();
-        noScrollMyGridView.setAdapter(adapter);
-        noScrollMyGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    private void initGridView(){
+        mgvPicturesGridView = (MyGridView) findViewById(R.id.mgv_pictures);
+        mgvPicturesGridView.setSelector(new ColorDrawable(Color.TRANSPARENT));
+        mgvPicturesAdapter = new GridAdapter(this);
+        mgvPicturesAdapter.update();
+        mgvPicturesGridView.setAdapter(mgvPicturesAdapter);
+        mgvPicturesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
                                     long arg3) {
@@ -167,131 +173,6 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
                 startActivity(intent);
             }
         });
-    }
-
-    @SuppressLint("HandlerLeak")
-    public class GridAdapter extends BaseAdapter {
-        private LayoutInflater inflater; // 视图容器
-        private int selectedPosition = -1;// 选中的位置
-        private boolean shape;
-
-        public boolean isShape() {
-            return shape;
-        }
-
-        public void setShape(boolean shape) {
-            this.shape = shape;
-        }
-
-        public GridAdapter(Context context) {
-            inflater = LayoutInflater.from(context);
-        }
-
-        public void update() {
-            loading();
-        }
-
-        public int getCount() {
-            return (Bimp.bmp.size() + 1);
-        }
-
-        public Object getItem(int arg0) {
-
-            return null;
-        }
-
-        public long getItemId(int arg0) {
-
-            return 0;
-        }
-
-        public void setSelectedPosition(int position) {
-            selectedPosition = position;
-        }
-
-        public int getSelectedPosition() {
-            return selectedPosition;
-        }
-
-        /**
-         * ListView Item设置
-         */
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder = null;
-            if (convertView == null) {
-
-                convertView = inflater.inflate(R.layout.item_published_grida,
-                        parent, false);
-                holder = new ViewHolder();
-                holder.image = (ImageView) convertView
-                        .findViewById(R.id.item_grida_image);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            if (position < Bimp.bmp.size() && null != Bimp.bmp && Bimp.bmp.size() > 0) {
-                holder.image.setVisibility(View.VISIBLE);
-                holder.image.setImageBitmap(Bimp.bmp.get(position));
-            } else {
-                holder.image.setVisibility(View.GONE);
-            }
-
-            return convertView;
-        }
-
-        public class ViewHolder {
-            public ImageView image;
-        }
-
-        Handler handler = new Handler() {
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case 1:
-                        adapter.notifyDataSetChanged();
-                        break;
-                }
-                super.handleMessage(msg);
-            }
-        };
-
-        public void loading() {
-            new Thread(new Runnable() {
-                public void run() {
-                    while (true) {
-                        if (Bimp.max == Bimp.drr.size()) {
-                            Message message = new Message();
-                            message.what = 1;
-                            handler.sendMessage(message);
-                            break;
-                        } else {
-                            try {
-                                String path = Bimp.drr.get(Bimp.max);
-                                System.out.println(path);
-                                Bitmap bm = Bimp.revitionImageSize(path);
-                                Bimp.bmp.add(bm);
-                                String newStr = path.substring(
-                                        path.lastIndexOf("/") + 1,
-                                        path.lastIndexOf("."));
-                                FileUtils.saveBitmap(bm, "" + newStr);
-                                Bimp.max += 1;
-                                Message message = new Message();
-                                message.what = 1;
-                                handler.sendMessage(message);
-                            } catch (IOException e) {
-
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            }).start();
-        }
-    }
-
-    protected void onRestart() {
-        adapter.update();
-        super.onRestart();
     }
 
     private void initMediaLayout() {
@@ -331,6 +212,12 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
         addVideoIcon.setBackgroundResource(R.drawable.bg_add_movie);
         llMediaTypes.addView(addVideoIcon);
     }
+
+    protected void onRestart() {
+        mgvPicturesAdapter.update();
+        super.onRestart();
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -434,5 +321,125 @@ public class BaseDiseaseDetailActivity extends AbstractActivity implements View.
     public void finish() {
         super.finish();
         Bimp.clearData();
+    }
+
+    @SuppressLint("HandlerLeak")
+    public class GridAdapter extends BaseAdapter {
+        private LayoutInflater inflater; // 视图容器
+        private int selectedPosition = -1;// 选中的位置
+        private boolean shape;
+
+        public boolean isShape() {
+            return shape;
+        }
+
+        public void setShape(boolean shape) {
+            this.shape = shape;
+        }
+
+        public GridAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void update() {
+            loading();
+        }
+
+        public int getCount() {
+            return (Bimp.bmp.size() + 1);
+        }
+
+        public Object getItem(int arg0) {
+
+            return null;
+        }
+
+        public long getItemId(int arg0) {
+
+            return 0;
+        }
+
+        public void setSelectedPosition(int position) {
+            selectedPosition = position;
+        }
+
+        public int getSelectedPosition() {
+            return selectedPosition;
+        }
+
+        /**
+         * ListView Item设置
+         */
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+
+                convertView = inflater.inflate(R.layout.item_published_grida,
+                        parent, false);
+                holder = new ViewHolder();
+                holder.image = (ImageView) convertView
+                        .findViewById(R.id.item_grida_image);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (position < Bimp.bmp.size() && null != Bimp.bmp && Bimp.bmp.size() > 0) {
+                holder.image.setVisibility(View.VISIBLE);
+                holder.image.setImageBitmap(Bimp.bmp.get(position));
+            } else {
+                holder.image.setVisibility(View.GONE);
+            }
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public ImageView image;
+        }
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1:
+                        mgvPicturesAdapter.notifyDataSetChanged();
+                        break;
+                }
+                super.handleMessage(msg);
+            }
+        };
+
+        public void loading() {
+            new Thread(new Runnable() {
+                public void run() {
+                    while (true) {
+                        if (Bimp.max == Bimp.drr.size()) {
+                            Message message = new Message();
+                            message.what = 1;
+                            handler.sendMessage(message);
+                            break;
+                        } else {
+                            try {
+                                String path = Bimp.drr.get(Bimp.max);
+                                System.out.println(path);
+                                Bitmap bm = Bimp.revitionImageSize(path);
+                                Bimp.bmp.add(bm);
+                                String newStr = path.substring(
+                                        path.lastIndexOf("/") + 1,
+                                        path.lastIndexOf("."));
+                                FileUtils.saveBitmap(bm, "" + newStr);
+                                Bimp.max += 1;
+                                Message message = new Message();
+                                message.what = 1;
+                                handler.sendMessage(message);
+                            } catch (IOException e) {
+
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }).start();
+        }
     }
 }
