b9bfcd7db5ecef8e9cdbc435c52c3f834cffe7b5
Janzon <orosem625@163.com>
2014-11-13 09:12:27 +0800

    逻辑错误修正

--- app/src/main/java/com/xiaohui/bridge/activity/DiseaseStatisticsActivity.java
+++ app/src/main/java/com/xiaohui/bridge/activity/DiseaseStatisticsActivity.java
@@ -30,97 +30,47 @@ public class DiseaseStatisticsActivity extends AbstractActivity {
         LinearLayout diseaseContentLayout = (LinearLayout) findViewById(R.id.ll_disease_content_layout);
         ComponentModel componentModel = (ComponentModel) getCookie().get(Keys.COMPONENT);
 
-        for(int i = 0; i < componentModel.getDiseases().size(); i ++){
-            DiseaseItemView itemView = new DiseaseItemView(this);
+        List<DiseaseModel> allDiseaseModels = componentModel.getDiseases();
+        Map<String, ArrayList<DiseaseModel>> diseasesMap = new HashMap<String, ArrayList<DiseaseModel>>();
+        // 将该部件下的所有病害分类
+        for (int j = 0; j < allDiseaseModels.size(); j++) {
+            DiseaseModel diseaseModel = allDiseaseModels.get(j);
+            String diseaseKey = diseaseModel.getDisease().getType() + diseaseModel.getDisease().getMethod();
+            // 判断这个病害的病害类型和输入方式在diseaseTypeList这个列表中是否存在，如果存在，那么将这个病害
+            // 放到他的集合中，如果不存在，那么新增一个集合
 
-            List<DiseaseModel> allDiseaseModels = componentModel.getDiseases();
+            if (diseasesMap.size() > 0) {
+                boolean isContain = false;
+                String sameDiseaseKey = "";
 
-//            // 所有的病害种类和病害录入方式的总的list
-//            ArrayList<Map<String, ArrayList<DiseaseModel>>> diseaseTypeList = new ArrayList<Map<String, ArrayList<DiseaseModel>>>();
-
-            Map<String, ArrayList<DiseaseModel>> diseasesMap = new HashMap<String, ArrayList<DiseaseModel>>();
-
-            // 将该部件下的所有病害分类
-            for(int  j = 0; j < allDiseaseModels.size(); j ++){
-                DiseaseModel diseaseModel = allDiseaseModels.get(i);
-                String diseaseKey = diseaseModel.getDisease().getType() + diseaseModel.getDisease().getMethod();
-                // 判断这个病害的病害类型和输入方式在diseaseTypeList这个列表中是否存在，如果存在，那么将这个病害
-                // 放到他的集合中，如果不存在，那么新增一个集合
-
-                if(diseasesMap.size() > 0){
-                    boolean isContain = false;
-                    String sameDiseaseKey = "";
-
-                    Iterator iterator = diseasesMap.entrySet().iterator();
-                    while (iterator.hasNext()) {
-                        Map.Entry entry = (Map.Entry) iterator.next();
-                        String key = (String)entry.getKey();
-                        if(key.equals(diseaseKey)){
-                            isContain = true;
-                            sameDiseaseKey = diseaseKey;
-                            break;
-                        }
+                Iterator iterator = diseasesMap.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry entry = (Map.Entry) iterator.next();
+                    String key = (String) entry.getKey();
+                    if (key.equals(diseaseKey)) {
+                        isContain = true;
+                        sameDiseaseKey = diseaseKey;
+                        break;
                     }
+                }
 
-                    if(isContain){
-                        ArrayList<DiseaseModel> models = diseasesMap.get(sameDiseaseKey);
-                        models.add(diseaseModel);
-                        diseasesMap.put(sameDiseaseKey, models);
-                    }
+                if (isContain) {
+                    ArrayList<DiseaseModel> models = diseasesMap.get(sameDiseaseKey);
+                    models.add(diseaseModel);
+                    diseasesMap.put(sameDiseaseKey, models);
                 } else {
                     ArrayList<DiseaseModel> models = new ArrayList<DiseaseModel>();
                     models.add(diseaseModel);
                     diseasesMap.put(diseaseKey, models);
                 }
-
-//                if(diseaseTypeList.size() > 0){
-//                    boolean isContain = false;
-//                    for(int x = 0; x < diseaseTypeList.size(); x ++){
-//                        Map<String, ArrayList<DiseaseModel>> curMap = diseaseTypeList.get(x);
-//                        Iterator iterator = curMap.entrySet().iterator();
-//                        while (iterator.hasNext()) {
-//                            Map.Entry entry = (Map.Entry) iterator.next();
-//                            String key = (String)entry.getKey();
-//                            if(key.equals(diseaseKey)){
-//                                isContain = true;
-//                                break;
-//                            }
-//                        }
-//                    }
-//
-//                    if(isContain){
-//                        for(int x = 0; x < diseaseTypeList.size(); x ++){
-//                            Map<String, ArrayList<DiseaseModel>> curMap = diseaseTypeList.get(x);
-//                            Iterator iterator = curMap.entrySet().iterator();
-//                            while (iterator.hasNext()) {
-//                                Map.Entry entry = (Map.Entry) iterator.next();
-//                                String key = (String)entry.getKey();
-//                                if(key.equals(diseaseKey)){
-//                                    ArrayList<DiseaseModel> diseaseModels = (ArrayList<DiseaseModel>) entry.getValue();
-//                                    diseaseModels.add(diseaseModel);
-//                                    curMap.put(diseaseKey, diseaseModels);
-//                                    break;
-//                                }
-//                            }
-//                        }
-//                    } else {
-//                        Map<String, ArrayList<DiseaseModel>> newMap = new HashMap<String, ArrayList<DiseaseModel>>();
-//                        newMap.put(diseaseKey, new ArrayList<DiseaseModel>());
-//                        diseaseTypeList.add(newMap);
-//                    }
-//                } else {
-//                    Map<String, ArrayList<DiseaseModel>> newMap = new HashMap<String, ArrayList<DiseaseModel>>();
-//                    newMap.put(diseaseKey, new ArrayList<DiseaseModel>());
-//                    diseaseTypeList.add(newMap);
-//                }
+            } else {
+                ArrayList<DiseaseModel> models = new ArrayList<DiseaseModel>();
+                models.add(diseaseModel);
+                diseasesMap.put(diseaseKey, models);
             }
-//            for(Map<String, ArrayList<DiseaseModel>> listMap : diseaseTypeList){
-//            }
-
-            // TODO 用分类的病害初始化视图
-
-//            diseaseContentLayout.addView(itemView.getInitDataView(componentModel.getDiseases().get(i).getDisease()));
         }
+        // TODO 用分类的病害初始化视图
+//      diseaseContentLayout.addView(itemView.getInitDataView(componentModel.getDiseases().get(i).getDisease()));
     }
 
 }
